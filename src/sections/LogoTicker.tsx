56010c62753ecfe997058136a65ddefb56794e79
Sourjya_1614 <159236410+Sourjya-Saha@users.noreply.github.com>
2025-02-23 07:32:12 +0530

    Update LogoTicker.tsx

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,95 +1,58 @@
 "use client";
-import acmeLogo from "@/assets/logo-acme.png";
-import quantumLogo from "@/assets/logo-quantum.png";
-import echoLogo from "@/assets/logo-echo.png";
-import celestialLogo from "@/assets/logo-celestial.png";
-import pulseLogo from "@/assets/logo-pulse.png";
-import apexLogo from "@/assets/logo-apex.png";
-import Image from "next/image";
-import { motion } from "framer-motion";
+
+import acmeLogo from '@/assets/sddefault-removebg-preview.png';
+import quantamLogo from '@/assets/VyvcKdbWHbTaN3QzRCQQS7pXASq1-303c31j4-removebg-preview.png';
+import echoLogo from '@/assets/kyKz5-removebg-preview.png';
+import celestialLogo from '@/assets/8k73hi03ck8mnm830xpu.png';
+import pulse from '@/assets/ETH_logo_landscape_(gray).png';
+import apexLogo from '@/assets/images__3_-removebg-preview (1).png';
+import sckitLogo from '@/assets/images__2_-removebg-preview.png';
+import pandas from '@/assets/images__1_-removebg-preview (1).png';
+import metamask from '@/assets/1_eu3r8jvsCHDGj4z5f8Nkxg-removebg-preview (1).png';
+import nextjs from '@/assets/nextjs-icon-2048x1234-pqycciiu.png';
+import Image from 'next/image';
+import {motion} from 'framer-motion';
+
 
 export const LogoTicker = () => {
-  return (
-    <div className="py-8 md:py-12 bg-white">
-      <div className="container">
-        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <motion.div
-            className="flex gap-14 flex-none pr-14"
-            animate={{
-              translateX: "-50%",
-            }}
-            transition={{
-              duration: 20,
-              repeat: Infinity,
-              ease: "linear",
-              repeatType: "loop",
-            }}
-          >
-            <Image
-              src={acmeLogo}
-              alt="Acme Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={quantumLogo}
-              alt="Quantum Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={echoLogo}
-              alt="Echo Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={celestialLogo}
-              alt="Celestial Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={pulseLogo}
-              alt="Pulse Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={apexLogo}
-              alt="Apex Logo"
-              className="logo-ticker-image"
-            />
+  return( <div className='py-8 md:py-12 bg-white'>
+    <div className="container">
+      <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
+      <motion.div className='flex gap-14 flex-none pr-14 ' animate={{
+        translateX:"-50%",
+      }}
+      transition={{
+        duration:12,
+        repeat:Infinity,
+        ease:"linear",
+        repeatType:"loop",
+      }}>
+        <Image src={acmeLogo} alt='AcmeLogo'  className='logo-ticker-image' />
+        <Image src={quantamLogo} alt='quantam Logo' className='logo-ticker-image'/>
+        <Image src={echoLogo} alt='echo Logo' className='logo-ticker-image'/>
+        <Image src={celestialLogo} alt='celestial Logo' className='logo-ticker-image'/>
+        <Image src={pulse} alt='pulse Logo' className='logo-ticker-image'/>
+        <Image src={apexLogo} alt='apex Logo' className='logo-ticker-image'/>
+        <Image src={sckitLogo} alt='apex Logo' className='logo-ticker-image'/>
+        <Image src={pandas} alt='apex Logo' className='logo-ticker-image'/>
+        <Image src={metamask} alt='apex Logo' className='logo-ticker-image'/>
+        <Image src={nextjs} alt='apex Logo' className='logo-ticker-image'/>
 
-            {/* Second set of logos for animation */}
-            <Image
-              src={acmeLogo}
-              alt="Acme Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={quantumLogo}
-              alt="Quantum Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={echoLogo}
-              alt="Echo Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={celestialLogo}
-              alt="Celestial Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={pulseLogo}
-              alt="Pulse Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={apexLogo}
-              alt="Apex Logo"
-              className="logo-ticker-image"
-            />
-          </motion.div>
-        </div>
+{/*2nd set off logo for anime */}
+        <Image src={acmeLogo} alt='AcmeLogo'  className='logo-ticker-image' />
+        <Image src={quantamLogo} alt='quantam Logo' className='logo-ticker-image'/>
+        <Image src={echoLogo} alt='echo Logo' className='logo-ticker-image'/>
+        <Image src={celestialLogo} alt='celestial Logo' className='logo-ticker-image'/>
+        <Image src={pulse} alt='pulse Logo' className='logo-ticker-image'/>
+        <Image src={apexLogo} alt='apex Logo' className='logo-ticker-image'/>
+        <Image src={sckitLogo} alt='apex Logo' className='logo-ticker-image'/>
+        <Image src={pandas} alt='apex Logo' className='logo-ticker-image'/>
+        <Image src={metamask} alt='apex Logo' className='logo-ticker-image'/>
+        <Image src={nextjs} alt='apex Logo' className='logo-ticker-image'/>
+      </motion.div>
       </div>
     </div>
+
+  </div>
   );
 };
